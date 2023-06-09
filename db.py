cf1223fa55ea4e08f0fe394263584cbfa342f610
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-09 15:21:41 +0500

    get data

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from tinydb import TinyDB
+from tinydb.table import Document
 
 db = TinyDB('db.json', indent=4)
 
-doc = {"name": "Ali Vali", "age": 20}
-
-db.insert(doc)
+docs = db.all()
+print(docs)
